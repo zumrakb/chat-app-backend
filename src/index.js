04600866ed51bf8ba892f09d57d82d1dc8d3fa5d
Zümra <zumrakb8@gmail.com>
2025-02-05 22:46:25 +0300

    add cors

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDB from "./lib/db.js";
+import cors from "cors";
 dotenv.config();
 const app = express();
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
